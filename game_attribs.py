13cad52ea0d5aa794ead22c5d1540b786e3e7df2
Dakota Rosell <rosell.dakota@gmail.com>
2022-05-13 18:11:07 -0400

    Create attribs dictionary with attributes and their possible values/meanings

---
+++ game_attribs.py
@@ -0,0 +1,13 @@
+"""
+Just messing around
+"""
+attribs = {
+    'Valiance' : 'Your ability to withstand demoralization and deathblows. Increases will-to-live',
+    'Condition' : 'The strength of you heart and lungs and their ability to work in extreme conditions. Increases will-to-act',
+    'Ingenuity' : 'How quickly you can devise solutions to imminent problems. Increases chance of critical rolls',
+    'Stability' : 'Balance, control, precision. Not only in body, but in mind. Increases will-to-fight',
+    'Communion' : 'Your connection to "other" things. Affects many things, most unseen'
+
+}
+
+print(attribs.keys())
