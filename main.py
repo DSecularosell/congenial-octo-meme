1e7fdca22451fdc1cb7beec1d53091a0e9d5d012
Dakota Secula-Rosell <rosell.dakota@gmail.com>
2022-05-14 23:07:46 -0400

    Update player positioning in main

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ def main():
     screen_width = 80
     screen_height = 50
 
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
+
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
@@ -17,7 +21,7 @@ def main():
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order='F')
         while True:
-            root_console.print(x=1,y=1, string='@')
+            root_console.print(player_x, player_y, string='@')
 
             context.present(root_console)
 
